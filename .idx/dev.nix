8e97d597ce65c9b08eae36e7a91017bb14dad9c7
iris <iris@heyiris.dev>
2025-02-05 11:44:19 +0000

    chore(env): fix nix file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,21 +1,15 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-24.05";
 
-  # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.python312
-    pkgs.python311Packages.pip
+    (pkgs.python312.withPackages (ps: with ps; [
+        pandas
+        ipykernel
+    ]))
   ];
 
-  # Sets environment variables in the workspace
-  env = {};
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
       "ms-python.debugpy"
       "ms-python.python"
       "ms-toolsai.jupyter"
@@ -24,35 +18,5 @@
       "ms-toolsai.vscode-jupyter-cell-tags"
       "ms-toolsai.vscode-jupyter-slideshow"
     ];
-
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        create-venv = "python -m venv venv";
-        install-packages = "./venv/bin/pip install pandas";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        
-      };
-    };
   };
 }
