04cd064935f4a9525875846576836948eb770a7b
iris <iris@heyiris.dev>
2025-02-10 11:50:47 +0000

    refac(wk3): move processing logic into separate function

--- src/wk3/02/activity_two.py
+++ src/wk3/02/activity_two.py
@@ -1,13 +1,18 @@
 import pandas as pd 
 
-df = pd.read_csv("./datasets/sdg_index_2000-2022_cp.csv")
 
-print(f"duplicates: {df.duplicated().sum()}")
-df.drop_duplicates(inplace=True)
+def process(dataframe):
+    print("processing dataset")
+    print(f"  found {dataframe.duplicated().sum()} duplicates")
+    print(f"  found {dataframe.isnull().sum().sum()} null values")
+    dataframe.drop_duplicates(inplace=True)    
+    dataframe.dropna(inplace=True)
+    return dataframe
 
-df2 = pd.read_csv("./datasets/sdg_report_2023(in).csv")
+df = process(pd.read_csv("./datasets/sdg_index_2000-2022_cp.csv"))
+df2 = process(pd.read_csv("./datasets/sdg_report_2023(in).csv"))
 
-df3 = pd.merge(df, df2, on=["country_code", "country"], )
+df3 = process(pd.merge(df, df2, on=["country_code", "country"]))
 df3.to_csv("./datasets/out.csv", index=False)
 
 print(df3.head())
