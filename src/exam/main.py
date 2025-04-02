9bbdfe4354a902b60ab90b1ddbf8a3b0e131fdef
iris <iris@heyiris.dev>
2025-04-02 13:23:29 +0000

    exam: clean up

--- src/exam/main.py
+++ src/exam/main.py
@@ -1,83 +1,154 @@
 import pandas as pd
+import os
+from datetime import datetime
+
+class Colours: 
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RESET = "\033[0m"
+
+class MainMenuChoice:
+    AVERAGE_INTERACTION = "1"
+    EXIT = "0"
+
+class AverageMenuChoice:
+    LIKES = "1"
+    SHARES = "2"
+    COMMENTS = "3"
+    BACK = "0"
+
+def clear_screen():
+    # if the user is on a unix-like system, use `clear`. otherwise, use `cls`
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def display_header(title):
+    clear_screen()
+    print("#" * 50)
+    print(f"{'# ' + title + ' #':^50}")
+    print("#" * 50)
+    print("")
+
+def get_validated_input(prompt, valid_options):
+    attempts = 0
+    current_prompt = prompt
+    while True:
+        choice = -1
+        if attempts > 0: choice = input(f" x {current_prompt}: ").strip()
+        else: choice = input(f" | {current_prompt}: ").strip()
+        
+        if choice in valid_options:
+            return choice
+        
+        error_msg = f"{Colours.RED}invalid option! please select from: {', '.join(valid_options)}{Colours.RESET} •"
+        current_prompt = f"{error_msg} {prompt}"
+        attempts += 1
 
 def main_menu():
-    flag = True
-
-    while flag:
-
-        print("#################################################")
-        print("############## Snowy Animal Rescue ##############")
-        print("#################################################")
-        print("")
-        print("########### Please select an option #############")
-        print("### 1. Average Social Media Interaction Data")
-
-        choice = input('Enter your number selction here: ')
-
-        try:
-            int(choice)
-        except:
-            print("Sorry, you did not enter a valid option")
-            flag = True
-        else:    
-            print('Choice accepted!')
-            flag = False
-
-    return choice
-
-
-def average_menu ():
-    flag = True
-
-    while flag:
-
-        print("#################################################")
-        print("############## Average Interaction ##############")
-        print("#################################################")
-        print("")
-        print("########### Please select an option #############")
-        print("### 1. Average number of Likes")   
-        print("### 2. Average number of Shares") 
-        print("### 3. Average number of Comments")  
-
-        choice = input('Enter your number selction here: ')
-
-        try:
-            int(choice)
-        except:
-            print("Sorry, you did not enter a valid option")
-            flag = True
-        else:    
-            print('Choice accepted!')
-            flag = False
-
-    return choice      
-
-def convert_avg_men_coice(avg_men_choice):
+    display_header("snowy animal rescue")
     
-    if avg_men_choice == "1":
-        avg_choice = "Likes"
-    elif avg_men_choice == "2":
-        avg_choice = "Shares"
-    else:
-        avg_choice = "Comments"  
+    print("please select an option:")
+    print(" 1. average social media interaction data")
+    print(" 0. exit program")
+    print("")
     
-    return avg_choice
+    return get_validated_input("enter your selection here", 
+                               [MainMenuChoice.AVERAGE_INTERACTION, 
+                                MainMenuChoice.EXIT])
 
-
-def get_avg_data(avg_choice):
+def average_menu():
+    display_header("average interactions")
     
-    df = pd.read_csv("Task4a_data.csv")
-    extract = df.groupby(['Date'], as_index=False) [avg_choice].mean()
-    extract_no_index = extract.to_string(index=False)
+    print("please select an option:")
+    print(" 1. average number of likes")   
+    print(" 2. average number of shares") 
+    print(" 3. average number of comments")
+    print(" 0. back to main menu")
+    print("")
     
-    print("Here is the average number of {} each day during the campaign:".format(avg_choice))
-    return extract_no_index
+    return get_validated_input("enter your selection here", 
+                             [AverageMenuChoice.LIKES, 
+                              AverageMenuChoice.SHARES, 
+                              AverageMenuChoice.COMMENTS,
+                              AverageMenuChoice.BACK])
 
-main_menu_choice = main_menu()
-if main_menu_choice == "1":
-    avg_men_choice = average_menu()
-    avg_choice = convert_avg_men_coice(avg_men_choice)
-    print(get_avg_data(avg_choice))
+# converts the average menu choice to the relevant column name in the 
+# dataframe.
+def convert_avg_menu_choice(avg_menu_choice):
+    choice_mapping = {
+        AverageMenuChoice.LIKES: "Likes",
+        AverageMenuChoice.SHARES: "Shares",
+        AverageMenuChoice.COMMENTS: "Comments"
+    }
+    mapping = choice_mapping.get(avg_menu_choice, "unknown")
+    if mapping == "unknown":
+        raise ValueError(f"invalid average menu choice: {avg_menu_choice}")
+    return mapping
 
+def load_data(filename="data.csv"):
+    try:
+        return pd.read_csv(filename)
+    except FileNotFoundError:
+        print(f"{Colours.RED}error: file '{filename}' not found.{Colours.RESET}")
+        return None
+    except pd.errors.EmptyDataError:
+        print(f"{Colours.RED}error: file '{filename}' is empty.{Colours.RESET}")
+        return None
+    except pd.errors.ParserError:
+        print(f"{Colours.RED}error: unable to parse '{filename}'.{Colours.RESET}")
+        return None
 
+def get_avg_data(avg_choice, df=None):
+    if df is None:
+        df = load_data()
+        
+    if df is None:
+        return f"{Colours.RED}error: no data available.{Colours.RESET}"
+    
+    try:
+        extract = df.groupby(['Date'], as_index=False)[avg_choice].mean()
+        
+        result = f"average number of {avg_choice} each day during the campaign:\n"
+        result += "-" * 45 + "\n"
+        result += f"{'Date':<12} | {avg_choice:>10}\n"
+        result += "-" * 45 + "\n"
+        
+        for _, row in extract.iterrows():
+            result += f"{row['Date']:<12} | {row[avg_choice]:>10.2f}\n"
+            
+        result += "-" * 45 + "\n"
+        result += f"overall average: {df[avg_choice].mean():.2f}\n"
+        
+        return result
+    except KeyError:
+        return f"{Colours.RED}error: column '{avg_choice}' wasn't found in the dataset.{Colours.RESET}"
+
+def main():
+
+    df = load_data()
+    if df is None:
+        print("couldn't load the date. exiting.")
+        return
+        
+    while True:
+        main_menu_choice = main_menu()
+        
+        if main_menu_choice == MainMenuChoice.EXIT:
+            print(" • exiting, goodbye!")
+            break
+            
+        elif main_menu_choice == MainMenuChoice.AVERAGE_INTERACTION:
+            while True:
+                avg_menu_choice = average_menu()
+                
+                if avg_menu_choice == AverageMenuChoice.BACK:
+                    break
+                    
+                avg_column = convert_avg_menu_choice(avg_menu_choice)
+                result = get_avg_data(avg_column, df)
+                
+                print("\n" + result)
+                input(f"\npress {Colours.GREEN}Enter{Colours.RESET} to continue...")
+
+if __name__ == "__main__":
+    main()
